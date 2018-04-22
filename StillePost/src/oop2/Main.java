35a11fe2e688665918bf3887a88f65fcdd01c54b
Paul Starke <paulstarke.ps@gmail.com>
2018-04-22 11:44:02 +0200

    a

--- StillePost/src/oop2/Main.java
+++ StillePost/src/oop2/Main.java
@@ -1,32 +1,13 @@
 package oop2;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> refs/heads/lala
 
 public class Main {
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
+		Eingabe e1 = new Eingabe();	
 		
-		
-=======
->>>>>>> refs/heads/lala
-		Eingabe e1 = new Eingabe();
-<<<<<<< HEAD
-		
-			e1.eingabe();
-=======
->>>>>>> refs/heads/lala
-		
-<<<<<<< HEAD
-		
-   
-
-		
-=======
 		e1.eingabe().spielablauf();;
->>>>>>> refs/heads/lala
+
 	}
 }
