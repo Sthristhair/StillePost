a80ca03e09047e7f87778681e9f5a37ae0cc1ed7
Sthristhair <32815941+Sthristhair@users.noreply.github.com>
2018-04-22 10:35:23 +0200

    xommit

--- StillePost/src/oop2/Main.java
+++ StillePost/src/oop2/Main.java
@@ -1,10 +1,7 @@
 package oop2;
 
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		
 		Gitter gitter = new Gitter(5,5,100,10);
 		
 		gitter.spielablauf();
