23294717b0b292c0a7ff07116c703d5c4bc7dfb9
Sthristhair <32815941+Sthristhair@users.noreply.github.com>
2018-04-22 10:36:40 +0200

    Immer deinen Mist ausbügeln hier

--- StillePost/src/oop2/Main.java
+++ StillePost/src/oop2/Main.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		
 		Scanner scan = new Scanner(System.in);
-=======
+
 		Gitter gitter = new Gitter(5,5,100,10);
->>>>>>> refs/heads/sonntag_morgen
 		
 		Eingabe e1 = new Eingabe();
 		while(true) {
